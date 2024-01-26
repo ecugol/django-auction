3a9d679c175b3a815119311dc3b62e5b2b3bdbcc
Ivan Vedernikov <ecugol@gmail.com>
2024-01-26 20:07:26 +0500

    fix: register models

--- auction/admin.py
+++ auction/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import BidItem
+
+from auction.models import Auction
+from auction.models import BidBasket
+from auction.models import BidItem
+from auction.models import Lot
 
 admin.site.register(BidItem)
+admin.site.register(Auction)
+admin.site.register(Lot)
+admin.site.register(BidBasket)
